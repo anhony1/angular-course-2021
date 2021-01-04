5c662f0c018af3a1f9d0e709edc3f9402a67b535
Anthony Reyes <42007836+anhony1@users.noreply.github.com>
2021-01-04 13:10:15 -0600

    Udemy commit
    making success and danger alerts

---
+++ src/app/alerts/danger/danger.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-danger',
+  templateUrl: './danger.component.html',
+  styleUrls: ['./danger.component.css']
+})
+export class DangerComponent implements OnInit {
+
+  name: string = 'WARNING BROTHER';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './alerts/sucess/success.component';
+import { DangerComponent } from './alerts/danger/danger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessComponent,
+    DangerComponent
   ],
   imports: [
     BrowserModule,
