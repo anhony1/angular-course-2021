4ca9b925d18115c8503e473a17836809eb599b94
Anthony Reyes <42007836+anhony1@users.noreply.github.com>
2021-01-11 11:55:57 -0600

    section 3
    finishing up section 3 moving on to section 4

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is just a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg'),new Recipe('A Test Recipe', 'This is just a test', 'https://www.glutenfreeandmore.com/wp-content/uploads/2018/07/10brisket.jpg')
+  ];
 
   constructor() { }
 
---
+++ src/app/recipe-book/recipe.model.ts
@@ -0,0 +1,18 @@
+
+
+export class Recipe {
+
+    //blueprint for objects we create
+
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, imagePath: string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imagePath;
+
+    }
+
+}
